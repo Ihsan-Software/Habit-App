c2d6ddb99820b2e284a37599c693905912169532
= <mu.arsalan19@gmail.com>
2023-08-20 01:36:44 +0300

    finish check, unchek, get today habit

--- controllers/habitController.js
+++ controllers/habitController.js
@@ -100,47 +100,167 @@ exports.getActiveHabits = catchAsync(async(req, res) => {
 
 exports.check = catchAsync(async(req, res) => {
 
-        const habit = await Habit.find({ name: req.body.name });
-        const result = await habit[0].checkProcess(habit)
-        if ( result[0]) {
-                const data = await result[0].getTodayHabitsProcess()
-                
-                res.status(200).json({
-                    status: 'success',
-                    requestTime:req.requestTime,
-                    activeCounter: data[1].length,
-                    notActiveCounter:data[0].length,
-                    data: {
-                        activeHabits: data[1],
-                        notActiveHabits:data[0]
-                        
-                    }
+    const habit = await Habit.find({ name: req.body.name });
+    console.log('start checkProcess')
+    req.requestTime = new Date().toISOString();
+    if (habit.length > 1) {
+
+        habit.forEach(ele => {
+            if (ele.active) {
+                ele.counter += 1;
+                ele.date.push(req.requestTime)
+                ele.save().catch((err) => {
+                    console.error('Error 🔥: ', err);
                 });
-        }
+            }
+        });
+        
+    }
+    else {
+
+        const newHabit = await Habit.create({
+            name: habit[0].name,
+            icon: habit[0].icon,
+            color: habit[0].color,
+            counter: 1,
+            active: true,
+            date: req.requestTime
+        });
+    }
+
+
+    var activeHabits = await Habit.find({ active: true });
+    var notActiveHabits = await Habit.find({ active: false });
+    const data = await activeHabits[0].getTodayHabitsProcess()
+    if (data[1][0] && data[0][0] ) {
+        res.status(200).json({
+            status: 'success',
+            requestTime:req.requestTime,
+            activeCounter: data[1].length,
+            notActiveCounter:data[0].length,
+            data: {
+                activeHabits: data[1],
+                notActiveHabits:data[0]
+                
+            }
+        });
+    }
+    else if (data[1].length==0 && data[0][0] ){
+        console.log('11111111111111')
+        data[1].push('Active Habits Is Empty')
+        console.log(data[1])
+        res.status(200).json({
+            status: 'success',
+            requestTime:req.requestTime,
+            activeCounter: data[1].length,
+            notActiveCounter:data[0].length,
+            data: {
+                activeHabits: data[1],
+                notActiveHabits:data[0]
+                
+            }
+        });
+    }
+    else if ((!data[0][0]) && data[1][0] ){
+        console.log('single habittttttttttt')
+        data[0].push('Not Active Habits Is Empty')
+        res.status(200).json({
+            status: 'success',
+            requestTime:req.requestTime,
+            activeCounter: data[1].length,
+            notActiveCounter:data[0].length,
+            data: {
+                activeHabits: data[1],
+                notActiveHabits:data[0]
+                
+            }
+        });
+    }
 
 });
 
 exports.unCheck = catchAsync(async (req, res) => {
 
-        const habit = await Habit.find({ name: req.body.name, active: true });
-        const result = await habit[0].unCheckProcess(habit)
+    const habit = await Habit.find({ name: req.body.name, active: true });
+    console.log('start unCheckProcess')
+    var tempHabit = []
+    currentTime = new Date().toISOString();
 
-        if ( result[0]) {
-            const data = await result[0].getTodayHabitsProcess()
-            
-            res.status(200).json({
-                status: 'success',
-                requestTime:req.requestTime,
-                activeCounter: data[1].length,
-                notActiveCounter:data[0].length,
-                data: {
-                    activeHabits: data[1],
-                    notActiveHabits:data[0]
-                    
-                }
+    habit[0].date.forEach(ele => {
+        if (ele.split('T')[0] !== currentTime.split('T')[0]) {
+            tempHabit.push(ele)
+        }
+    })
+
+    habit[0].date = habit[0].date.filter(item => item.split('T')[0] === currentTime.split('T')[0])
+
+    if (habit[0].date[0] && habit[0].date[0].split('T')[0] === currentTime.split('T')[0]) {
+
+
+        if (habit[0].counter > 1) {
+            habit[0].date = habit[0].date.filter(item => item.split('T')[0] !== currentTime.split('T')[0])
+
+            habit[0].counter -= 1;
+            tempHabit.forEach(ele => {
+                habit[0].date.push(ele)
+            })
+            habit[0].save().catch((err) => {
+                console.error('Error 🔥: ', err);
             });
+
+        }
+        else 
+            await Habit.findByIdAndDelete(habit[0]._id);
     }
-        
+
+
+    var activeHabits = await Habit.find({ active: true });
+    var notActiveHabits = await Habit.find({ active: false });
+    const data = await activeHabits[0].getTodayHabitsProcess()
+    if (data[1][0] && data[0][0] ) {
+        res.status(200).json({
+            status: 'success',
+            requestTime:req.requestTime,
+            activeCounter: data[1].length,
+            notActiveCounter:data[0].length,
+            data: {
+                activeHabits: data[1],
+                notActiveHabits:data[0]
+                
+            }
+        });
+    }
+    else if (data[1].length==0 && data[0][0] ){
+        console.log('11111111111111')
+        data[1].push('Active Habits Is Empty')
+        console.log(data[1])
+        res.status(200).json({
+            status: 'success',
+            requestTime:req.requestTime,
+            activeCounter: data[1].length,
+            notActiveCounter:data[0].length,
+            data: {
+                activeHabits: data[1],
+                notActiveHabits:data[0]
+                
+            }
+        });
+    }
+    else if ((!data[0][0]) && data[1][0] ){
+        console.log('single habittttttttttt')
+        data[0].push('Not Active Habits Is Empty')
+        res.status(200).json({
+            status: 'success',
+            requestTime:req.requestTime,
+            activeCounter: data[1].length,
+            notActiveCounter:data[0].length,
+            data: {
+                activeHabits: data[1],
+                notActiveHabits:data[0]
+                
+            }
+        });
+    }
 });
 
 exports.getTodayHabits = catchAsync(async(req, res) => {
@@ -148,9 +268,8 @@ exports.getTodayHabits = catchAsync(async(req, res) => {
 
         var activeHabits = await Habit.find({ active: true });
         var notActiveHabits = await Habit.find({ active: false });
-        if (activeHabits[0]) {
-            const data = await activeHabits[0].getTodayHabitsProcess()
-            
+        const data = await activeHabits[0].getTodayHabitsProcess()
+        if (data[1][0] && data[0][0] ) {
             res.status(200).json({
                 status: 'success',
                 requestTime:req.requestTime,
@@ -163,17 +282,37 @@ exports.getTodayHabits = catchAsync(async(req, res) => {
                 }
             });
         }
-        else {
+        
+        else if (data[1].length==0 && data[0][0] ){
+            console.log('11111111111111')
+            data[1].push('Active Habits Is Empty')
+            console.log(data[1])
             res.status(200).json({
                 status: 'success',
                 requestTime:req.requestTime,
-                activeCounter: activeHabits.length,
-                notActiveCounter:notActiveHabits.length,
+                activeCounter: data[1].length,
+                notActiveCounter:data[0].length,
                 data: {
-                    notActiveHabits
+                    activeHabits: data[1],
+                    notActiveHabits:data[0]
                     
                 }
             });
         }
-
+            
+        else if ((!data[0][0]) && data[1][0] ){
+            console.log('single habittttttttttt')
+            data[0].push('Not Active Habits Is Empty')
+            res.status(200).json({
+                status: 'success',
+                requestTime:req.requestTime,
+                activeCounter: data[1].length,
+                notActiveCounter:data[0].length,
+                data: {
+                    activeHabits: data[1],
+                    notActiveHabits:data[0]
+                    
+                }
+            });
+        }
 })
